c394bbef6b749468762faa491914475baa51d52a
Héctor Martín López <hectoriko@gmail.com>
2021-07-15 19:03:59 +0200

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
   refinedMoonstone: 4,
 };
 
+const elevenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 6,
+};
+
 console.log(elvenShieldRecipe);
+console.log(elevenGauntletsRecipe);
